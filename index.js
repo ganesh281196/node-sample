87e0f53397ad01172a0f1df97a6c191a83c43202
ganesh281196 <ganesh.kumar@zapcg.com>
2022-10-11 16:45:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/get", function(req,res) {
+res.send('This is the test')
+} )
+
+app.get("/", function(req,res) {
+    res.send('Welcome to home page')
+    } );
+
+app.listen(port, () => console.log(`app running in port ${port}`))
