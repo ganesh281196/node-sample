3df8f67e9b12b061df0e36ef7c6e5e8c0f8eedc2
ganesh281196 <ganesh.kumar@zapcg.com>
2022-10-11 16:48:06 +0530

    ping

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/get", function(req,res) {
+app.get("/ping", function(req,res) {
 res.send('This is the test')
 } )
 
